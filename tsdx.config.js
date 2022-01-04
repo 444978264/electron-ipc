be93103ea21a28358691b6f0481ef5206ead2465
debugger-007 <yi444978264@gmail.com>
2022-01-04 17:46:18 +0800

    test

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,6 +1,10 @@
+const peerDeps = require('rollup-plugin-peer-deps-external');
+
 module.exports = {
   rollup(config, options) {
-    config.external = ['electron'];
+    // config.plugins.unshift(peerDeps());
+    // console.log(config, 'config.plugins');
+    // config.external = ['electron'];
     return config;
   },
 };
